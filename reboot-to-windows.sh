8b9c4cfb3d26734dea7f3a27aecd5cb0b6aa7464
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-12 23:47:04 +0100

    Added TODO

--- reboot-to-windows.sh
+++ reboot-to-windows.sh
@@ -1,3 +1,5 @@
+#TODO: Add functionality to dynamically detect the bootnum of the Windows partition.
+
 set -e #Halts the program if error occurs (i.e., not getting sudo permission)
 
 pkexec efibootmgr -n 0000
