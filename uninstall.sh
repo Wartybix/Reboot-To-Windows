b1e4df1de6d877296c5a5ce59091213161e3f98e
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-13 00:25:33 +0100

    Uninstaller removes program's directory in /usr/share/

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
-sudo rm /usr/share/reboot-to-windows/windows_logo.png
+sudo rm -rf /usr/share/reboot-to-windows/
 sudo rm /usr/local/bin/reboot-to-windows.sh
 sudo rm /usr/share/applications/windows.desktop
