eeb055a8e03c0a86fb3e357a1e75e1dd6c7105a2
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-12 21:22:44 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo cp windows_logo.svg /usr/share/icons/hicolor/scalable/apps/
+sudo cp reboot-to-windows.sh /usr/local/bin/
+sudo cp windows.desktop /usr/share/applications/
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+sudo rm /usr/share/icons/hicolor/scalable/apps/windows_logo.svg
+sudo rm /usr/local/bin/reboot-to-windows.sh
+sudo rm /usr/share/applications/windows.desktop
