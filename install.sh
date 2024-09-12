8873712b3b57e8685e644752512710c5aacc4760
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-12 23:13:59 +0100

    Desktop file now correctly shows an icon

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-sudo cp windows_logo.svg /usr/share/icons/hicolor/scalable/apps/
+sudo mkdir -p /usr/share/reboot-to-windows
+sudo cp windows_logo.png /usr/share/reboot-to-windows/
 sudo cp reboot-to-windows.sh /usr/local/bin/
 sudo cp windows.desktop /usr/share/applications/
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
-sudo rm /usr/share/icons/hicolor/scalable/apps/windows_logo.svg
+sudo rm /usr/share/reboot-to-windows/windows_logo.png
 sudo rm /usr/local/bin/reboot-to-windows.sh
 sudo rm /usr/share/applications/windows.desktop
