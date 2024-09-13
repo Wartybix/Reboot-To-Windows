564ee448068244b87d285fc63cec7469fac70544
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-13 12:26:58 +0100

    Added message to install qt if using KDE.
    Message is shown if the user is running KDE when running installer.

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ sudo mkdir -p /usr/share/reboot-to-windows
 sudo cp windows_logo.png /usr/share/reboot-to-windows/
 sudo cp reboot-to-windows.sh /usr/local/bin/
 sudo cp wartybix-windows.desktop /usr/share/applications/
+
+if [ $DESKTOP_SESSION == plasma ]; then
+	echo "Ensure the 'qt' package or similar is installed from your package manager to provide support for KDE's reboot prompt."
+fi
