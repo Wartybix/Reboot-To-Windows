0f67728b1d627043e562f89bc161bfdf426cf1d0
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-13 14:49:00 +0100

    Changed $DESKTOP_ENVIRONMENT to $DESKTOP_SESSION.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo cp windows_logo.png /usr/share/reboot-to-windows/ # Icon
 sudo cp reboot-to-windows.sh /usr/local/bin/ # Executable
 sudo cp wartybix-windows.desktop /usr/share/applications/ # Desktop file
 
-if [ "$DESKTOP_ENVIRONMENT" = "plasma" ]; then # If user running KDE Plasma:
+if [ "$DESKTOP_SESSION" = "plasma" ]; then # If user running KDE Plasma:
 	# Check for 'qdbus' command. If empty, set to -1.
 	QDBUS=`which qdbus 2>/dev/null || echo -1`
 	if [ "$QDBUS" = "-1" ]; then # If qdbus command not found:
