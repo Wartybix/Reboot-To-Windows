29a92d159ef1237ea0d070d16248013abba4bdc0
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-13 13:34:47 +0100

    Installer checks for existence of qdbus command.
    Installer now only instructs the user to install qt when the qdbus
    command is not found.

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ sudo cp windows_logo.png /usr/share/reboot-to-windows/ # Icon
 sudo cp reboot-to-windows.sh /usr/local/bin/ # Executable
 sudo cp wartybix-windows.desktop /usr/share/applications/ # Desktop file
 
-if [ $DESKTOP_SESSION == plasma ]; then # If user running KDE Plasma:
-	echo "Ensure the 'qt' package or similar is installed from your package manager to provide support for KDE's reboot prompt."
+if [ $DESKTOP_SESSION==plasma ]; then # If user running KDE Plasma:
+	# Check for 'qdbus' command. If empty, set to -1.
+	QDBUS=command -v qdbus || echo -1
+	if [ $QDBUS==-1 ]; then # If qdbus command not found:
+		echo "Install the 'qt' package or similar from your package manager to provide support for KDE's reboot prompt."
+	fi
 fi
