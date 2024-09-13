5423e5d7508ddb8614ceef315b69f062c4cca47a
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-13 11:38:07 +0100

    Changed desktop file name to include vendor prefix.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo mkdir -p /usr/share/reboot-to-windows
 sudo cp windows_logo.png /usr/share/reboot-to-windows/
 sudo cp reboot-to-windows.sh /usr/local/bin/
-sudo cp windows.desktop /usr/share/applications/
+sudo cp wartybix-windows.desktop /usr/share/applications/
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 sudo rm -rf /usr/share/reboot-to-windows/
 sudo rm /usr/local/bin/reboot-to-windows.sh
-sudo rm /usr/share/applications/windows.desktop
+sudo rm /usr/share/applications/wartybix-windows.desktop
