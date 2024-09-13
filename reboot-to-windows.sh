3fe1b246fb3b4dec7ebbbca0d3eaeff8cb8eae05
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-13 10:30:14 +0100

    Added support for different desktop environments.

--- reboot-to-windows.sh
+++ reboot-to-windows.sh
@@ -3,4 +3,11 @@
 set -e #Halts the program if error occurs (i.e., not getting sudo permission)
 
 pkexec efibootmgr -n 0000
-gnome-session-quit --reboot
+
+case $DESKTOP_SESSION in
+	"gnome")
+		gnome-session-quit --reboot
+	"kde")
+		qdbus org.kde.ksmserver /KSMServer logout 1 1 1
+	*)
+		reboot
