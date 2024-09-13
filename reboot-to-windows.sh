6082b1366a3f1641eb678a0c3b890654ca0c2f78
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-13 11:58:51 +0100

    Changed reboot script to avoid syntax errors.
    Added bash shebang to the top of the file.
    WINDOWS_BOOT_NUMBER variable assignment is fixed.

--- reboot-to-windows.sh
+++ reboot-to-windows.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 #TODO: Add functionality to dynamically detect the bootnum of the Windows partition.
 
 set -e #Halts the program if error occurs (i.e., not getting sudo permission)
 
-WINDOWS_BOOT_NUMBER = efibootmgr | \ # Get list of boot options.
-grep -i Windows | \ 		     # Search for line containing 'Windows' in
-				     # these options (case insensitive).
-grep -Eo "Boot(.*?\*)" | \ 	     # Get 'Boot____*' value of this line.
-grep -Eo "[0-9]{4}" 		     # Extract the 4-digit number from this
-				     # value.
+# Gets list of boot options.
+# Then searches for line containing 'Windows' in these options (case
+# insensitive).
+# Gets the 'Boot____*' value of this line.
+# Extracts the 4-digit number from this value.
+WINDOWS_BOOT_NUMBER=efibootmgr | grep -i Windows | grep -Eo "Boot(.*?\*)" | grep -Eo "[0-9]{4}"
 
 pkexec efibootmgr -n $WINDOWS_BOOT_NUMBER
 
