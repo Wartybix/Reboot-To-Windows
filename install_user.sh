79dbdc646896e10863c0b03636176b2fff04d656
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-15 16:10:28 +0100

    Moved local icon location.

--- install_user.sh
+++ install_user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/.icons
-cp windows_logo.png ~/.icons # Icon
+mkdir -p ~/.local/share/icons
+cp windows_logo.png ~/.local/share/icons # Icon
 mkdir -p ~/.local/bin/
 cp reboot-to-windows.sh ~/.local/bin # Executable
 cp wartybix-windows-user.desktop ~/.local/share/applications # Desktop file
