aa400fcb91ed71d33917bbb5de62ba949ebff5a4
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-26 00:53:02 +0100

    Changed main script location in installers.
    Reflected change made in PKGBUILD (to place the main script in /usr/bin,
    rather than /usr/local/bin) into manual installer/uninstaller scripts.

--- install_root.sh
+++ install_root.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir -p /usr/share/reboot-to-windows # Supporting files
 sudo cp windows_logo.png /usr/share/reboot-to-windows/ # Icon
-sudo cp reboot-to-windows.sh /usr/local/bin/ # Executable
+sudo cp reboot-to-windows.sh /usr/bin/reboot-to-windows # Executable
 sudo cp wartybix-windows-root.desktop /usr/share/applications/ # Desktop file
 
 bash kde_check.sh
--- uninstall_from_root.sh
+++ uninstall_from_root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo rm -rf /usr/share/reboot-to-windows/ # Supporting files
-sudo rm /usr/local/bin/reboot-to-windows.sh # Executable
+sudo rm /usr/bin/reboot-to-windows # Executable
 sudo rm /usr/share/applications/wartybix-windows-root.desktop # Desktop file
