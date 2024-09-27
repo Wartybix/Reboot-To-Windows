a5a0b1924d9f485c34ffb3dd4b15cfd010d25371
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-27 06:20:17 +0100

    Fixed desktop file creation in user installer.
    Ensured that home applications folder is created if not already
    existing.

--- install_user.sh
+++ install_user.sh
@@ -4,6 +4,7 @@ mkdir -p ~/.local/share/icons
 cp windows_logo.png ~/.local/share/icons # Icon
 mkdir -p ~/.local/bin/
 cp reboot-to-windows.sh ~/.local/bin # Executable
+mkdir -p ~/.local/share/applications/
 cp wartybix-windows-user.desktop ~/.local/share/applications # Desktop file
 
 sh kde_check.sh
