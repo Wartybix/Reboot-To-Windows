46a22d3ccef59f0f28ebeb2853a146d6ab9154e5
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-27 05:19:29 +0100

    No more absolute paths in root .desktop file.

--- install_root.sh
+++ install_root.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo mkdir -p /usr/share/reboot-to-windows # Supporting files
-sudo cp windows_logo.png /usr/share/reboot-to-windows/ # Icon
+sudo cp windows_logo.png /usr/share/pixmaps/ # Icon
 sudo cp reboot-to-windows.sh /usr/bin/reboot-to-windows # Executable
 sudo cp wartybix-windows-root.desktop /usr/share/applications/ # Desktop file
 
--- uninstall_from_root.sh
+++ uninstall_from_root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo rm -rf /usr/share/reboot-to-windows/ # Supporting files
+sudo rm /usr/share/pixmaps/windows_logo.png # Icon
 sudo rm /usr/bin/reboot-to-windows # Executable
 sudo rm /usr/share/applications/wartybix-windows-root.desktop # Desktop file
